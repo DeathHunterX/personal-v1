c00d8398a2f08c45c916ea910ce539e8f2045c4e
Loi Phan <88315858+DeathHunterX@users.noreply.github.com>
2024-11-05 14:54:14 +0700

    update metadata

--- frontend/src/data/AppMetadata.tsx
+++ frontend/src/data/AppMetadata.tsx
@@ -1,10 +1,9 @@
 const author = "Loi Phan Thanh";
-const description =
-  "Software developer from Moldova, Republic of, who loves to develop beautiful websites, single page applications, customer relationship management or code from scratch using React and Next.js";
-const url = "https://vasile-novatchii.netlify.app";
+const description = "";
+const url = "https://loiphan-nextjs-portfolio.netlify.app";
 
 export const AppMetadata = {
-  metadataBase: new URL("http://localhost:3000"), // Corrected URL format
+  metadataBase: new URL(url), // Corrected URL format
   title: {
     default: `Portfolio | ${author}`,
     template: `%s | ${author}`,
@@ -15,14 +14,14 @@ export const AppMetadata = {
   },
   keywords: [
     "Loi Phan Thanh",
-    "Loi Phan Thanh - software developer",
     "Frontend developer",
+    "Data Engineer",
     "ReactJS developer",
     "Portfolio website",
     "Frontend Developer Portfolio",
   ],
   creator: author,
-  authors: [{ name: author, url }],
+  authors: [{ name: author, description, url }],
   openGraph: {
     title: `${author} | Portfolio`,
     description,
@@ -30,13 +29,13 @@ export const AppMetadata = {
     siteName: `${author} | Portfolio`,
     images: [
       {
-        url: "https://vasile-novatchii.netlify.app/screenshot.webp",
+        url: "https://loiphan-nextjs-portfolio.netlify.app/screenshot.png",
         width: 800,
         height: 600,
         alt: "My personal portfolio website",
       },
       {
-        url: "https://vasile-novatchii.netlify.app/screenshot.webp",
+        url: "https://loiphan-nextjs-portfolio.netlify.app/screenshot.png",
         width: 1800,
         height: 1600,
         alt: "My personal portfolio website",
